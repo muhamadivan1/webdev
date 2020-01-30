c4e9c68da6657c3f908dce09bcda19d6a050cbde
muhamadivan1 <muhammadivan174@gmail.com>
2020-01-30 21:07:48 +0700

    modul 5

---
+++ latihan js.js
@@ -0,0 +1,38 @@
+var variabel1 = "asdf"
+var variabel2 = 29.1
+
+let variabel3 = 12
+
+variabel4 = 99
+
+console.log(variabel4)
+
+//cara1
+function namaFungsi(){
+
+}
+
+var namaFungsi2 = function(){
+
+}
+
+var namaFungsi3 = () => {
+    
+}
+
+var namaFungsi3 = (x) => {
+console.log(x)   
+}
+
+() => {
+
+}
+
+
+var tambah = (x,y) => {
+    return x + y
+}
+
+console.log(tambah(5,6))
+
+console.log(variabel3)
